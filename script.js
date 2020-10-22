83ac7964b1c83021ff4bd20541d19671b4a6897a
SaiMaheshwarReddy <maheshwarreddysai@gmail.com>
2020-10-22 19:06:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const nextBtn = document.querySelector(".next")
+const welcomeContainer = document.querySelector(".welcome__container")
+const loginContainer = document.querySelector(".login__container")
+const backBtn = document.querySelector(".back__btn")
+const registerBtn = document.querySelector(".register__btn")
+const registerNowBtn = document.querySelector(".register__now__btn")
+const registerContainer = document.querySelector(".register__container")
+const registerBackBtn = document.querySelector(".register__back__btn")
+const loginNowBtn = document.querySelector(".login__now__btn")
+
+const home = document.querySelector(".home")
+
+
+nextBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    welcomeContainer.classList.add("trx2");
+    loginContainer.classList.add("trx")
+})
+
+backBtn.addEventListener("click", (e) => {
+e.preventDefault()
+loginContainer.classList.remove("trx")
+welcomeContainer.classList.remove("trx2");
+})
+
+registerNowBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    // console.log("clicked")
+    loginContainer.classList.remove("trx2")
+    registerContainer.classList.add("trx");
+    })
+
+registerBackBtn.addEventListener("click", (e) => {
+e.preventDefault()
+registerContainer.classList.remove("trx");
+loginContainer.classList.add("trx")
+})
+
+
+
+registerBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+registerContainer.classList.remove("trx2");
+home.classList.add("trx")
+})
