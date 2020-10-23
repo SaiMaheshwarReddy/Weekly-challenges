2affe118ea8c0b1c2952863da07e156194da2cfc
SaiMaheshwarReddy <61627080+SaiMaheshwarReddy@users.noreply.github.com>
2020-10-23 17:11:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ const registerContainer = document.querySelector(".register__container")
 const registerBackBtn = document.querySelector(".register__back__btn")
 const loginNowBtn = document.querySelector(".login__now__btn")
 const loginBtn = document.querySelector(".login__btn")
+const input = document.querySelector("input")
 
 const home = document.querySelector(".home")
+
+
+
+   
+
 
 
 nextBtn.addEventListener("click", (e) => {
@@ -52,14 +58,21 @@ loginNowBtn.addEventListener("click", (e) => {
 
 registerBtn.addEventListener("click", (e) => {
     e.preventDefault()
+    if(input.value == ""){
+        alert("incorrect input")
+    }else{
     home.classList.remove("trx")
 registerContainer.classList.add("trx2");
 home.classList.add("trx")
+    }
 })
 
 loginBtn.addEventListener('click', (e) => {
     e.preventDefault()
+    if(input.value == ""){
+        alert("incorrect input")
+    }else{
     loginContainer.classList.add("trx2");
     home.classList.add("trx");
-
+    }
 })
