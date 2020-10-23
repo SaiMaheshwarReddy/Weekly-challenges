137aa07b1dfd8012484ff3601e8aa1f35d6f3e89
SaiMaheshwarReddy <61627080+SaiMaheshwarReddy@users.noreply.github.com>
2020-10-23 08:06:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const registerNowBtn = document.querySelector(".register__now__btn")
 const registerContainer = document.querySelector(".register__container")
 const registerBackBtn = document.querySelector(".register__back__btn")
 const loginNowBtn = document.querySelector(".login__now__btn")
+const loginBtn = document.querySelector(".login__btn")
 
 const home = document.querySelector(".home")
 
 
 nextBtn.addEventListener("click", (e) => {
     e.preventDefault()
+   
     welcomeContainer.classList.add("trx2");
     loginContainer.classList.add("trx")
 })
@@ -24,22 +26,40 @@ welcomeContainer.classList.remove("trx2");
 })
 
 registerNowBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    // console.log("clicked")
-    loginContainer.classList.remove("trx2")
+ 
+    registerContainer.classList.remove("trx2");
+    registerContainer.classList.remove("trx");
+    loginContainer.classList.add("trx2")
     registerContainer.classList.add("trx");
     })
 
 registerBackBtn.addEventListener("click", (e) => {
 e.preventDefault()
-registerContainer.classList.remove("trx");
-loginContainer.classList.add("trx")
+registerContainer.classList.remove("trx2");
+registerContainer.classList.add("trx2");
+loginContainer.classList.remove("trx2");
 })
+
+loginNowBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+   
+
+    registerContainer.classList.add("trx2");
+    loginContainer.classList.remove("trx2");
+    })
 
 
 
 registerBtn.addEventListener("click", (e) => {
     e.preventDefault()
-registerContainer.classList.remove("trx2");
+    home.classList.remove("trx")
+registerContainer.classList.add("trx2");
 home.classList.add("trx")
+})
+
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    loginContainer.classList.add("trx2");
+    home.classList.add("trx");
+
 })
